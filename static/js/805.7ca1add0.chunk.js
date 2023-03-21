2ed2c8e2cc1d7eb930d60d677dde7a8e3f85b5fb
sergeyalekseev1976 <109554680+sergeyalekseev1976@users.noreply.github.com>
2023-03-21 19:55:54 +0000

    Deploying to gh-pages from @ sergeyalekseev1976/goit-react-hw-05-movies@3ae081b56e31dea5b131a529f56cbc8e131686e3 🚀

---
+++ static/js/805.7ca1add0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,t,n){n.r(t);var r=n(1087),c=n(184);t.default=function(){return(0,c.jsxs)("main",{style:{textAlign:"center"},children:[(0,c.jsx)("b",{style:{fontSize:64},children:"404"}),(0,c.jsx)("p",{children:"Sorry, we couldn't find that page"}),(0,c.jsx)(r.rU,{to:"/",children:"Bach to Homepage"})]})}}}]);
+//# sourceMappingURL=805.7ca1add0.chunk.js.map
